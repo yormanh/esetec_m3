65d2b7362d1b52eef0eca249b5411d0b077bcf87
Yorman <yormanh@gmail.com>
2023-07-18 15:30:02 +0100

    Terrenos. Personaje Enemigo. Arboles. Viento. Vectores. materiales.

---
+++ 3DIntro/Assets/MyAssets/Scripts/EsferaEnemiga.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EsferaEnemiga : MonoBehaviour
+{
+    public float velocidad;
+    private GameObject _player;
+    public float distanciaMinima;
+
+    private void Start()
+    {
+        _player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        //this.transform.LookAt(_player.transform, Vector3.up); 
+
+        //a quien quiero mirar - desde donde quiero mirar (desde donde estoy)
+        Vector3 direction = _player.transform.position - this.transform.position;
+        //direction.Normalize();
+        transform.rotation = Quaternion.LookRotation(direction);
+
+        //Si queremos que avanza en z
+        //transform.position += new Vector3(0,0,1);
+        //transform.position += direction.normalized * velocidad * Time.deltaTime;
+
+        if (Mathf.Abs(direction.z) > distanciaMinima)
+        {
+            transform.position += direction.normalized * velocidad * Time.deltaTime;
+        }
+
+
+    }
+}
